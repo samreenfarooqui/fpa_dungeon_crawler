f8e3e3227da940dcdff5f42a1392be4c813ff209
samreenfarooqui <samreen.farooqui678@gmail.com>
2025-01-22 18:09:37 -0500

    added logout button and functionality

--- app/src/main/java/com/example/cs2340c_team38/views/MainActivity.java
+++ app/src/main/java/com/example/cs2340c_team38/views/MainActivity.java
@@ -7,14 +7,20 @@ import androidx.lifecycle.ViewModelProvider;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cs2340c_team38.R;
 import com.example.cs2340c_team38.databinding.ActivityMainBinding;
 import com.example.cs2340c_team38.viewmodels.MainViewModel;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
     private MainViewModel viewModel;
     private MediaPlayer mediaPlayer;
+
+    private Button buttonLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
         );
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         binding.setViewModel(viewModel);
+
+        buttonLogout = findViewById(R.id.buttonLogout);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+
 
         viewModel.getStartEvent().observe(this, message -> startActivity(
                 new Intent(MainActivity.this, ConfigActivity.class)
@@ -37,4 +52,11 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer = MediaPlayer.create(this, R.raw.planning);
         mediaPlayer.start();
     }
+    public void logoutUser(){
+        FirebaseAuth.getInstance().signOut();
+        mediaPlayer.stop();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
+    }
+
 }
